081504ac5c603691f539342304eed70787e683bc
Mrl4n0r <lanamau368@gmail.com>
2026-04-27 09:00:52 +0800

    Latihan 1 - Membuat Controller Baru

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,19 +1,19 @@
-<?php 
- 
-use CodeIgniter\Router\RouteCollection; 
- 
-/** 
- * @var RouteCollection $routes 
- */ 
- 
-// Route default (halaman beranda) 
-$routes->get('/', 'Beranda::index'); 
- 
-// Route halaman tentang 
-$routes->get('tentang', 'Beranda::tentang'); 
- 
-// Route dengan parameter numerik 
-$routes->get('pengguna/(:num)', 'Beranda::pengguna/$1'); 
- 
-// Route halaman waktu 
+<?php
+use CodeIgniter\Router\RouteCollection;
+/**
+* @var RouteCollection $routes
+*/
+// Route default (halaman beranda)
+$routes->get('/', 'Beranda::index');
+$routes->get('welcome', 'Beranda::index');
+// Route halaman tentang
+$routes->get('tentang', 'Beranda::tentang');
+// Route dengan parameter numerik
+$routes->get('pengguna/(:num)', 'Beranda::pengguna/$1');
+// Route halaman waktu
 $routes->get('waktu', 'Beranda::waktu');
+
+// routes latihan 1
+$routes->get('akademik', 'Akademik::index');
+$routes->get('matkul', 'Akademik::matkul');
+$routes->get('nilai/(:alphanum)', 'Akademik::nilai/$1');
